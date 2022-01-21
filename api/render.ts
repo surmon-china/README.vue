70d7915846bdf54c6be80ff85668bbd2b7fbafd8
surmon-china <surmon.me@gmail.com>
2022-01-22 00:30:25 +0800

    feat: header type

--- api/render.ts
+++ api/render.ts
@@ -4,7 +4,7 @@ import { renderTemplate } from '../app/render'
 export default function (request: VercelRequest, response: VercelResponse) {
   renderTemplate(request.query)
     .then((svg) => {
-      response.setHeader('Content-Type', 'application/xml')
+      response.setHeader('Content-Type', 'image/svg+xml')
       response.send(svg)
     })
     .catch((error) => {
