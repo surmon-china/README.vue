ab06baa067e3773c1715322f0d622542dfebbaca
surmon-china <surmon.me@gmail.com>
2022-01-22 00:38:02 +0800

    feat: header & end

--- api/render.ts
+++ api/render.ts
@@ -4,14 +4,11 @@ import { renderTemplate } from '../app/render'
 export default function (request: VercelRequest, response: VercelResponse) {
   renderTemplate(request.query)
     .then((svg) => {
-      response.setHeader('Content-Type', 'image/svg+xml')
-      response.send(svg)
+      response.setHeader('Content-Type', 'image/svg+xml').end(svg)
     })
     .catch((error) => {
-      const message = error?.message || String(error)
-      response.status(500)
-      response.send({
-        error: message
+      response.status(500).send({
+        error: error?.message || String(error)
       })
     })
 }
