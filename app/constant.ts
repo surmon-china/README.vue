668d1f57ef8f417246162afa610798b5c5b84e3b
surmon-china <surmon.me@gmail.com>
2022-01-30 05:15:37 +0800

    feat: devicon

--- app/constant.ts
+++ app/constant.ts
@@ -4,10 +4,12 @@ import fs from 'fs-extra'
 export const GITHUB_TOKEN = process.env.GH_DEV_TOKEN as string
 export const MONGODB_URI = process.env.MONGODB_URI as string
 
-export const TEMPLATES_PATH = path.resolve(__dirname, '..', 'templates')
+export const ROOT_PATH = path.resolve(__dirname, '..')
+
+export const TEMPLATES_PATH = path.resolve(ROOT_PATH, 'templates')
 export const TEMPLATES_NAME = fs.readdirSync(TEMPLATES_PATH)
 
-export const COMPONENTS_PATH = path.resolve(__dirname, '..', 'components')
+export const COMPONENTS_PATH = path.resolve(ROOT_PATH, 'components')
 export const COMPONENTS_NAME = fs.readdirSync(COMPONENTS_PATH)
 
-export const PACKAGE_JSON = fs.readJsonSync(path.resolve(__dirname, '..', 'package.json'))
+export const PACKAGE_JSON = fs.readJsonSync(path.resolve(ROOT_PATH, 'package.json'))
